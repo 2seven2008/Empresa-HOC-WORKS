ba67a81d3bccce6255fdbcdd7386db99d2cc27e4
FelipeNovais <162530029+KanekaLipe@users.noreply.github.com>
2025-11-12 15:13:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,10 +18,11 @@ self.addEventListener("install", (event) => {
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
+
 });
